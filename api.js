3b998b9373b3b2fe87a45525988073eabb037c58
Bardia Rastin <a.rastin98@gmail.com>
2018-03-28 13:37:11 +0430

    adds long process support

--- api.js
+++ api.js
@@ -1,9 +1,32 @@
-const exec = require("child_process").exec;
+const { exec, spawn } = require("child_process");
 const fs = require("fs");
 
 const processes = {};
 
 const options = { maxBuffer: 1024 * 1000000 };
+
+let longRuningProcesses = {};
+
+const startLongRuningProcess = (script) => {
+  try {
+    const commandProcess = spawn('npm', ['run', script]);
+    longRuningProcesses[script] = commandProcess
+    commandProcess.stdout.on('data', (data) => {
+      console.log(`stdout: ${data}`);
+    });
+    
+    commandProcess.stderr.on('data', (data) => {
+      console.log(`std error data: ${data}`);
+    });
+    
+    commandProcess.on('close', (code) => {
+      console.log(`child process exited with code ${code}`);
+    });
+    return Promise.resolve('async process started successfully')
+  } catch (error) {
+    return Promise.reject(error)
+  }
+}
 
 module.exports = {
   routes: {
@@ -26,7 +49,19 @@ module.exports = {
         if (!body || !body.script) {
           return res.send(false);
         }
-
+        const appRoot = req.app.locals.appRoot;
+        const webdashJson = require(`${appRoot}/webdash.json`);
+        const { longRunningScripts = [] } = webdashJson
+        if (~longRunningScripts.indexOf(body.script)) {
+          startLongRuningProcess(body.script)
+          .then((result) => {
+            res.send({ response: result });
+          })
+          .catch((error) => {
+            return res.status(400).send({ error: error.toString() });
+          })
+          return;
+        }
         processes[body.script] = exec(
           `npm run ${body.script} --silent`,
           options,
@@ -39,6 +74,12 @@ module.exports = {
         );
       },
       stop: (req, res) => {
+        const appRoot = req.app.locals.appRoot;
+        const webdashJson = require(`${appRoot}/webdash.json`);
+        if (~longRunningScripts.indexOf(body.script)) {
+          longRuningProcesses[script].kill();
+          return res.send(true);
+        }
         const body = req.body;
         if (!body || !body.script) {
           return res.send(false);
